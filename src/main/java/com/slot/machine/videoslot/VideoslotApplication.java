b9cd3ab86dd145a0ca9e664c8a3283149fbd214a
bkravchenko <bkravchenko@playtika.com>
2019-09-16 14:10:32 +0300

    First commit

---
+++ src/main/java/com/slot/machine/videoslot/VideoslotApplication.java
@@ -0,0 +1,13 @@
+package com.slot.machine.videoslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoslotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoslotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slot/machine/videoslot/VideoslotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.slot.machine.videoslot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoslotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
